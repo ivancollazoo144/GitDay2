bae99045d1c0d3e9900d75fd862a07b0006d9019
ivancollazoo <collazoivan1412@gmail.com>
2025-01-31 19:44:22 -0400

    added three methods

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -10,4 +10,16 @@ public class DemoDay2 {
 		System.out.println(a+b);
 	}
 
+	public void add(int a, int b) {
+		System.out.println(a + b);
+	}
+	
+	public void sub(int a, int b) {
+		System.out.println(a - b);
+	}
+	
+	public void div(int a, int b) {
+		System.out.println(a / b);
+	}
+	
 }// this is coming from github
